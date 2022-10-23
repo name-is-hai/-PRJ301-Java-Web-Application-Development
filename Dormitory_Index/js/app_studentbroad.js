f1fe6d80eb4be6f98797c723d60c38258e3d72e6
Vũ Đào Ngọc Hải <haiqd065@gmail.com>
2022-10-23 13:57:23 +0700

    history

--- Dormitory_Index/js/app_studentbroad.js
+++ Dormitory_Index/js/app_studentbroad.js
@@ -1,1 +1,6 @@
-
+$(document).ready(function () {
+    $(".hamburger_wrap").click(function () {
+        $(this).parent().toggleClass("active");
+        $(".dd_list").toggleClass("active");
+    });
+});
